2ea98e7654275aa8b939729ae59ed32b44cb3052
bukharkan <bukharkan@gmail.com>
2020-03-19 23:14:58 +0700

    updated add organization

--- Logistics/App_Start/IdentityConfig.cs
+++ Logistics/App_Start/IdentityConfig.cs
@@ -54,11 +54,11 @@ namespace Logistics
             // Configure validation logic for passwords
             manager.PasswordValidator = new PasswordValidator
             {
-                RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
-                RequireDigit = true,
-                RequireLowercase = true,
-                RequireUppercase = true,
+                RequiredLength = 6
+                //RequireNonLetterOrDigit = true,
+                //RequireDigit = true,
+                //RequireLowercase = true,
+                //RequireUppercase = true,
             };
 
             // Configure user lockout defaults
